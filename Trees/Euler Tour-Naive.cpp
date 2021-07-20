59ca3e70e6c69adb926fa644291a084853d088d4
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-07-20 15:28:02 +0530

    Update Euler Tour-Naive.cpp

--- Trees/Euler Tour-Naive.cpp
+++ Trees/Euler Tour-Naive.cpp
@@ -1,5 +1,7 @@
-/go to node,print it.exit the node,print it.
+//go to node,print it.exit the node,print it.
 //there will be 2*n nodes which will be printed.Here,we are starting from the root.So root, will also be printed twice
+
+Method 1:
 void dfs1(int curr,int par){
 	//time in of curr node
 	cout<<curr<<" ";
@@ -15,6 +17,8 @@ void dfs1(int curr,int par){
 //go to a node,print it.come back to a node,print it.
 //when we are coming to an edge we are printing it.SO (n-1) edges.So,print it 2*n-1 times.
 //here root node will be printed only 1 time So 2*n-1 prints.
+
+Method 2:
 void dfs2(int curr,int par){
 	cout<<curr<<" ";
 	for(auto x:adj[curr]){
