20eb73eedf8710dec8b372dfa44719a6aa7f16ea
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-07-22 01:17:24 +0530

    Create LCA-Binary Lifting.cpp

---
+++ Trees/LCA-Binary Lifting.cpp
@@ -0,0 +1,3 @@
+// Here,Sparse table will store 2^i th parent of every node.
+// The differnce between the depth of the two nodes can be written in Power of 2.
+// Par[i][j] -> Represents 2^j th parent of i. 
