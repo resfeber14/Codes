2d8b4af44e6f6a0070a5ab80d002d04756354137
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-07-22 15:17:31 +0530

    Delete Maximum Depth of Binary Tree.cpp

--- Trees/Maximum Depth of Binary Tree.cpp
+++
@@ -1,9 +0,0 @@
-// Problem Link: https://leetcode.com/problems/maximum-depth-of-binary-tree/
-
-int maxDepth(TreeNode* root) {
-        if(!root)
-            return 0;
-        return max(maxDepth(root->left),maxDepth(root->right))+1;
- }
-
-
