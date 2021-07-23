44d9b3e42b6f3b5d51a2a7d7868b95b5a7b8534c
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-07-23 12:58:00 +0530

    Create Binary Tree Pruning.cpp

---
+++ Trees problems/Binary Tree Pruning.cpp
@@ -0,0 +1,25 @@
+bool solve(TreeNode* root){
+        if(!root)
+            return true;
+        bool l=solve(root->left);
+        bool r=solve(root->right);
+        if(l && r){
+            root->left=root->right=NULL;
+            if(root->val==0)
+                return true;
+        }
+        else{
+            if(l)
+                root->left=NULL;
+            if(r)
+                root->right=NULL;
+        }
+        return false;
+        
+    }
+    TreeNode* pruneTree(TreeNode* root) {
+        bool x= solve(root);
+        if(x)
+            return NULL;
+        return root;
+    }
