b71d9ec9029620536ee6844a490e737245139ad8
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-08-12 16:13:12 +0530

    Update MIinimum no of jumps to reach end.cpp

--- Dynamic Programming/MIinimum no of jumps to reach end.cpp
+++ Dynamic Programming/MIinimum no of jumps to reach end.cpp
@@ -1,10 +1,10 @@
-// Problem Link:   https://practice.geeksforgeeks.org/problems/minimum-number-of-jumps-1587115620/1#
+// Problem Link:   https://leetcode.com/problems/jump-game-ii/
 
 int minJumps(int arr[], int n){
         int curr_max=0,curr_end=0,jumps=0;
         for(int i=0;i<n;i++){
             curr_max=max(curr_max,i+arr[i]);
-            if(i==curr_end){
+            if(i==curr_end && i!=n-1){  // i!=n-1 is to ensure if array is [0] then answer will be 0 jumps:)
                 jumps++;
                 curr_end=curr_max;
             }
