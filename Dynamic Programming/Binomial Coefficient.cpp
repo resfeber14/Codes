345260654e306fd4829058dcbb113f655e91455c
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-08-12 12:32:49 +0530

    Create Binomial Coefficient.cpp

---
+++ Dynamic Programming/Binomial Coefficient.cpp
@@ -0,0 +1,22 @@
+// Problem Link:  https://practice.geeksforgeeks.org/problems/ncr1019/1#
+
+// nCr= (n-1)C(r-1) + (n-1)Cr
+    int solve(int n,int r,vector<vector<int> > &dp){
+        if(n==r){
+            return 1;
+        }
+        if(r==1){
+            return n;
+        }
+        if(dp[n][r]!=-1)
+            return dp[n][r];
+        dp[n][r]=((solve(n-1,r-1,dp))%1000000007+(solve(n-1,r,dp))%1000000007)%1000000007;
+        return dp[n][r];
+    }
+    int nCr(int n, int r){
+        // code here
+        if(n<r)
+            return 0;
+        vector<vector<int> > dp(n+1,vector<int> (r+1,-1));
+        return solve(n,r,dp);
+    }
