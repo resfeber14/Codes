37e667bdf5011a630a047fabacabbeba8fc18290
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-08-13 14:57:28 +0530

    Create Maximum difference of zeros and ones in binary string .cpp

---
+++ Dynamic Programming/Maximum difference of zeros and ones in binary string .cpp
@@ -0,0 +1,25 @@
+// Modification of Kadane
+// Convert all 0s to 1 and all 1s to -1.Then find maximum subarray sum
+
+int maxSubstring(string s)
+	{
+	    int n=s.length();
+	    int curr=0,ans=INT_MIN;
+	    for(int i=0;i<n;i++){
+	        int temp=0;
+	        if(s[i]=='1'){
+	            temp= -1;
+	        }
+	        else{
+	            temp=1;
+	        }
+	        if(temp+curr>temp){
+	            curr=temp+curr;
+	        }
+	        else{
+	            curr=temp;
+	        }
+	        ans=max(ans,curr);
+	    }
+	    return ans;
+	}
