910a35c3056967f12055aa705ad7d92598de27b3
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-08-24 09:35:39 +0530

    Update Perfect Squares.cpp

--- Dynamic Programming/Perfect Squares.cpp
+++ Dynamic Programming/Perfect Squares.cpp
@@ -28,3 +28,20 @@ int solve(int i,vector<int> &dp){
         return x;
     }
 
+
+// Bottom Up:
+
+int numSquares(int n) {
+    int dp[n+1];
+    dp[0]=0;
+    if(n<=2)
+       return n;
+    dp[1]=1,dp[2]=2;
+    for(int i=3;i<=n;i++){
+       dp[i]=INT_MAX;
+         for(int j=1;j*j<=i;j--){
+               dp[i]=min(dp[i-j*j]+1,dp[i]);
+          }
+     }
+     return dp[n];
+    }
