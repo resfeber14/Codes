3367de9518bdf30a8729241b2dcf09d60a0a987b
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-09-11 17:22:38 +0530

    Update Prim's Algo.cpp

--- Graphs/Prim's Algo.cpp
+++ Graphs/Prim's Algo.cpp
@@ -1,5 +1,6 @@
 //Time Complexity: O((N+E)+ N*logN)
-
+// In this algo,we take the nodes one by one and insert all its adjacent nodes in the priority queue and every time we take that edge from the priority queue which hasn't been
+// discovered yet and has minimum weight among all the nodes in the pq
 void solve(vector<int> &key,vector<int> &mst,vector<int> &parent,vector<vector<int>> adj[],int V){
     priority_queue <pair<int,int>, vector<pair<int,int>>, greater<pair<int,int> > > pq;
     pq.push({0,0});
@@ -9,7 +10,7 @@ void solve(vector<int> &key,vector<int> &mst,vector<int> &parent,vector<vector<i
         mst[u]=1;
         for(auto child: adj[u]){
             int v=child[0], wt= child[1];
-            if(!mst[v] && wt<key[v]){   // !mst[v] is to check a cycle
+            if(!mst[v] && wt<key[v]){   // !mst[v] is to check a cycle,i.e. it checks if node v is considered or not 
                 parent[v]=u,key[v]=wt;
                 pq.push({key[v],v});
             }
