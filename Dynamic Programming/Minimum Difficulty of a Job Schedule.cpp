f6e5ecfa872840e684f07b7bc743fdcdd71438f9
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-09-22 01:05:16 +0530

    Create Minimum Difficulty of a Job Schedule.cpp

---
+++ Dynamic Programming/Minimum Difficulty of a Job Schedule.cpp
@@ -0,0 +1,28 @@
+// Problem Link: https://leetcode.com/problems/minimum-difficulty-of-a-job-schedule/
+
+// Just Like MCM
+
+int solve(int i,int j,vector<int> &arr,vector<vector<int> > &dp){
+        if(i==arr.size() && j==0)
+            return 0;
+        if(i==arr.size() && j>=1)
+            return INT_MAX;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        int op1=INT_MAX,maxx=arr[i],ans=INT_MAX;
+        for(int k=i;k<arr.size();k++){
+            maxx=max(arr[k],maxx);
+            if(j>=1)
+            op1=solve(k+1,j-1,arr,dp);
+            if(op1!=INT_MAX)
+                ans=min(op1+maxx,ans);
+        }
+        return dp[i][j]= ans;
+    }
+    int minDifficulty(vector<int>& arr, int d) {
+        int n=arr.size();
+        if(n<d)
+            return -1;
+        vector<vector<int> > dp(n,vector<int> (d+1,-1));
+        return solve(0,d,arr,dp);
+    }
