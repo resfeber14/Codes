7658d90e66603e38058ec5c6f55ffa96557d3726
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-09-25 11:40:48 +0530

    Update Subarray with B odd numbers.cpp

--- Hashing/Subarray with B odd numbers.cpp
+++ Hashing/Subarray with B odd numbers.cpp
@@ -1,5 +1,8 @@
 // Problem Link:  https://www.interviewbit.com/problems/subarray-with-b-odd-numbers/
 
+// Here,we are replacing odd numbers with 1 and even with 0.
+// If count ==b ,then we increment the count and also include those subarrays which were counted earlier
+
 int Solution::solve(vector<int> &arr, int k) {
     int n=arr.size();
     int ans=0;
