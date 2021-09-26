7e72281dc8097160a023a28ea4ed5b4ef3aa24f6
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-09-26 14:57:06 +0530

    Update Count Number of Nice Subarrays.cpp

--- Hashing/Count Number of Nice Subarrays.cpp
+++ Hashing/Count Number of Nice Subarrays.cpp
@@ -1,8 +1,7 @@
 // Problem Link: https://leetcode.com/problems/count-number-of-nice-subarrays/
 
-// Here we are calculating running sum of no of odd elements ,and storing thr count of no of odd elements in a map.
-// And whenver we have got summ
-
+// Here we are calculating running sum of no of odd elements ,and storing thr count of no of odd elements in a map. Now this question reduces to https://leetcode.com/problems/subarray-sum-equals-k/
+// Here we are checking if m[sum-k] odd elments have been found earlier,if yes then that of subarrays we can get whose sum==k(i,.e. no of odd no =k)
 
 int numberOfSubarrays(vector<int>& arr, int k) {
         int n=arr.size();
