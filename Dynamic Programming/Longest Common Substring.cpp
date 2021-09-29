5db33d1308ea945116250d0ec5ac9576f3f6d8b4
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-09-29 10:33:36 +0530

    Update Longest Common Substring.cpp

--- Dynamic Programming/Longest Common Substring.cpp
+++ Dynamic Programming/Longest Common Substring.cpp
@@ -38,12 +38,7 @@ int longestCommonSubstr (string s1, string s2, int n, int m)
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(s1[i-1]==s2[j-1]){
-                    if(dp[i-1][j-1]){
-                        dp[i][j]=dp[i-1][j-1]+1;
-                    }
-                    else{
-                        dp[i][j]=1;
-                    }
+                    dp[i][j]=dp[i-1][j-1]+1;
                 }
                 else{
                     dp[i][j]=0;
