44eb342b498e3845275a022dd94fd745fe8dbc18
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-09-29 10:16:26 +0530

    Update Pairs with specific difference .cpp

--- Dynamic Programming/Pairs with specific difference .cpp
+++ Dynamic Programming/Pairs with specific difference .cpp
@@ -1,6 +1,40 @@
 // Problem Link:  https://practice.geeksforgeeks.org/problems/pairs-with-specific-difference1533/1#
 // Just sort the array.And check if(arr[i]-arr[i-1]<k) then dp[i]=dp[i-2]+arr[i]+arr[i-1] 
 
+// Top Down:
+
+int solve(int i,int arr[],int k,int n,vector<int> &dp){
+        if(i==n)
+            return 0;
+        if(dp[i]!=-1)
+            return dp[i];
+        int x=upper_bound(arr,arr+n,arr[i]+k-1)-arr;
+        if(x==n){
+            x--;
+        }
+        if(arr[x]-arr[i]>=k)
+            x--;
+        int op1=0;
+        if(x>i && arr[x]-arr[i]<k){
+            for(int j=i+1;j<=x;j++){
+                op1=max(arr[j]+arr[i]+solve(j+1,arr,k,n,dp),op1);
+            }
+        }
+        int op2=solve(i+1,arr,k,n,dp);
+       // op1=max(,op1);
+       return dp[i]=max(op1,op2);
+    }
+    int maxSumPairWithDifferenceLessThanK(int arr[], int n, int k)
+    {
+        // Your code goes here   
+        sort(arr,arr+n);
+        vector<int> dp(n,-1);
+        return solve(0,arr,k,n,dp);
+    }
+
+
+// Bottom UP
+
 int maxSumPairWithDifferenceLessThanK(int arr[], int n, int k){
         int dp[n];
         dp[0]=0;
