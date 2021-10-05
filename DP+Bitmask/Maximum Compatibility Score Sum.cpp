78ee6d4efd7c0900f237f1857e15165cf807c3f9
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-10-05 11:04:15 +0530

    Create Maximum Compatibility Score Sum.cpp

---
+++ DP+Bitmask/Maximum Compatibility Score Sum.cpp
@@ -0,0 +1,35 @@
+/* Problem Link: https://leetcode.com/problems/maximum-compatibility-score-sum/
+Time Complexity: O(N*M*2^N) Space Complexity: O(2^N)
+
+*/
+
+
+int dp[1<<9]; // shows how many students have been assigned any mentor
+    
+    int solve(int mask,vector<vector<int> > &students,vector<vector<int> > &mentors,int n){
+        int k=__builtin_popcount(mask);
+        if(k==n)  // If all the students have been assigned a mentor
+            return 0;
+        if(dp[mask]!=-1)
+            return dp[mask];
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if((mask & (1<<i))==0){ // ith student has not be assigned any mentor,then we can assign kth mentor to the ith student
+                int cnt=0;
+                for(int j=0;j<students[i].size();j++){
+                    if((students[i][j])^(mentors[k][j])==0){
+                        cnt++;
+                    }
+                }
+                ans=max(ans,cnt+solve(mask|(1<<i),students,mentors,n));
+            } 
+        }
+        return dp[mask]=ans;
+    }
+    
+    
+    int maxCompatibilitySum(vector<vector<int>>& students, vector<vector<int>>& mentors) {
+        int n=students.size();
+        memset(dp,-1,sizeof(dp));
+        return solve(0,students,mentors,n);
+    }
