ce3c31d0cf2ce20f11727c4d4344da0c7a96082d
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-10-08 00:19:57 +0530

    Update Stone Game III.cpp

--- Dynamic Programming/Stone Game III.cpp
+++ Dynamic Programming/Stone Game III.cpp
@@ -34,3 +34,40 @@ int solve(int i,vector<int> &arr,int n,vector<int> &dp){
             return "Alice";
         return "Bob";
     }
+
+
+// More Diverse Approach
+
+int solve(int i,vector<int> &arr,int n,vector<int> &dp){
+        if(i>=arr.size())
+            return 0;
+        if(dp[i]!=INT_MIN)
+            return dp[i];
+        int sum=0,ans=INT_MIN;
+        for(int j=i;j<min(i+3,n);j++){  // Alice's turn
+            sum+=arr[j];
+            int temp=INT_MAX;
+            for(int k=j+1;k<min(j+4,n);k++){ // If Bob will take the kth element
+                temp=min(temp,solve(k+1,arr,n,dp)); // He will always try to give the piles which will give the minimum points
+            } 
+            if(temp==INT_MAX)
+                temp=0;
+          dp[i]=max(temp+sum,dp[i]); // Alice will always pick such that she gets the maximum among those minimum left by BOB
+        }
+        return dp[i];
+    }
+    string stoneGameIII(vector<int>& arr) {
+        int n=arr.size();
+        int sum=0;
+        for(int i=0;i<n;i++)
+            sum+=arr[i];
+        vector<int> dp(n,INT_MIN);
+        int x=solve(0,arr,n,dp);
+        
+        cout<<x<<"\n";
+        if(x==sum-x)
+            return "Tie";
+        if(x>sum-x)
+            return "Alice";
+        return "Bob";
+    }
