24ce39e6b4c61ce598647ee5c06e24bd76cee8b3
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-10-10 20:39:46 +0530

    Update Stone Game VII.cpp

--- Dynamic Programming/Stone Game VII.cpp
+++ Dynamic Programming/Stone Game VII.cpp
@@ -1,7 +1,7 @@
 /*
 
 Problem Link:  https://leetcode.com/problems/stone-game-vii/
-
+In dp(i,j),we are storing the maximum difference of the points of Alice and Bob if they both play optimally
 
 
 */
@@ -12,24 +12,24 @@ int solve(int i,int j,int sum,vector<int> &arr,vector<vector<int> > &dp){
         if(dp[i][j]!=-1)
             return dp[i][j];
         int ans1=0,ans2=0;
-        int op=solve(i+1,j-1,sum-arr[i]-arr[j],arr,dp);
-        int op1=op+(sum-arr[i])-(sum-arr[i]-arr[j]);
-        int op2=solve(i+2,j,sum-arr[i]-arr[i+1],arr,dp)+(sum-arr[i])-(sum-arr[i]-arr[i+1]);
-        if(op1>op2){
+        int op=solve(i+1,j-1,sum-arr[i]-arr[j],arr,dp);  
+        int op1=op+(sum-arr[i])-(sum-arr[i]-arr[j]); // If alice picks arr[i] and bob picks arr[j] 
+        int op2=solve(i+2,j,sum-arr[i]-arr[i+1],arr,dp)+(sum-arr[i])-(sum-arr[i]-arr[i+1]);  // If alice picks arr[i] and bob picks arr[i+1]
+        if(op1>op2){ // Bob will always try to leave that group of piles which will later result in minimum of the maximum difference between the alice and bob scores
             ans1=op2;
         }
         else{
             ans1=op1;
         }
-        int op3=op+(sum-arr[j])-(sum-arr[j]-arr[i]);
-        int op4=solve(i,j-2,sum-arr[j-1]-arr[j],arr,dp)+(sum-arr[j])-(sum-arr[j]-arr[j-1]);
-        if(op3>op4){
+        int op3=op+(sum-arr[j])-(sum-arr[j]-arr[i]); // If alice picks arr[j] and bob picks arr[i] 
+        int op4=solve(i,j-2,sum-arr[j-1]-arr[j],arr,dp)+(sum-arr[j])-(sum-arr[j]-arr[j-1]); // If alice picks arr[j] and bob picks arr[j-1] 
+        if(op3>op4){ // Bob will always try to leave that group of piles which will later result in minimum of the maximum difference between the alice and bob scores
             ans2=op4;
         }
         else{
             ans2=op3;
         }
-        int ans= max(ans1,ans2);
+        int ans= max(ans1,ans2);  // ALice will try to maximise the difference of the points 
         return dp[i][j]= ans;
     }
     
