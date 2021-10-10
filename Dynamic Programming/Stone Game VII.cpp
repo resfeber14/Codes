45de0d354aa297c4807da45f886f344016110b8f
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-10-10 20:33:57 +0530

    Update Stone Game VII.cpp

--- Dynamic Programming/Stone Game VII.cpp
+++ Dynamic Programming/Stone Game VII.cpp
@@ -16,22 +16,17 @@ int solve(int i,int j,int sum,vector<int> &arr,vector<vector<int> > &dp){
         int op1=op+(sum-arr[i])-(sum-arr[i]-arr[j]);
         int op2=solve(i+2,j,sum-arr[i]-arr[i+1],arr,dp)+(sum-arr[i])-(sum-arr[i]-arr[i+1]);
         if(op1>op2){
-          //  ans1= (sum-arr[i])-(sum-arr[i]-arr[i+1])+op2;
             ans1=op2;
         }
         else{
-          //  ans1= (sum-arr[i])-(sum-arr[i]-arr[j])+op1;
             ans1=op1;
         }
-        
         int op3=op+(sum-arr[j])-(sum-arr[j]-arr[i]);
         int op4=solve(i,j-2,sum-arr[j-1]-arr[j],arr,dp)+(sum-arr[j])-(sum-arr[j]-arr[j-1]);
         if(op3>op4){
-          //  ans2=(sum-arr[j])-(sum-arr[j]-arr[j-1])+op4;
             ans2=op4;
         }
         else{
-            //ans2=(sum-arr[j])-(sum-arr[j]-arr[i])+op3;
             ans2=op3;
         }
         int ans= max(ans1,ans2);
@@ -45,7 +40,5 @@ int solve(int i,int j,int sum,vector<int> &arr,vector<vector<int> > &dp){
             sum+=stones[i];
         vector<vector<int> > dp(n,vector<int> (n,-1));
         int x= solve(0,n-1,sum,stones,dp);
-       // cout<<x<<" "<<sum-x<<"\n";
-        //return x-(sum-x);
         return x;
     }
