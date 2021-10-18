0452861837da02c485493a9d889715e29a5209b9
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-10-18 13:29:12 +0530

    Update Subtree with Maximum Average.cpp

--- Trees problems/Subtree with Maximum Average.cpp
+++ Trees problems/Subtree with Maximum Average.cpp
@@ -2,6 +2,9 @@
 Problem Link: https://binarysearch.com/problems/Subtree-with-Maximum-Average
 */
 
+
+
+double res=INT_MIN;
 pair<int,int> solve1(Tree* root){ // returns a pair which contains {total sum of the subtree starting from root , no of nodes in the subtree of root}
     pair<int,int> p={0,0};
     if(!root){
@@ -13,13 +16,14 @@ pair<int,int> solve1(Tree* root){ // returns a pair which contains {total sum of
     auto r=solve1(root->right);
     p.first=l.first+r.first+root->val;
     p.second=1+l.second+r.second;
+    res=max(res,(double)p.first/(double)p.second);
     return p;
 }
 
 double solve(Tree* root) {
     if(!root)
         return 0.0;
+    res=INT_MIN;
     pair<int,int> p=solve1(root);
-    double ans= double(p.first)/double(p.second);
-    return ans= max(max(solve(root->left),solve(root->right)),ans);
+    return res;
 }
