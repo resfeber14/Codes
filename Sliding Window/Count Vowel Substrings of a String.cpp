912808dfa6234ab13cb77b056d06c0db20392ee9
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-11-08 00:15:52 +0530

    Update Count Vowel Substrings of a String.cpp

--- Sliding Window/Count Vowel Substrings of a String.cpp
+++ Sliding Window/Count Vowel Substrings of a String.cpp
@@ -1,7 +1,7 @@
 /*
 Problem Link:  https://leetcode.com/problems/count-vowel-substrings-of-a-string/
 */
-
+// Approach 1: Find all the substrings in O(N*N*N) and check if the current substring can contribute to answer or not
 // Time Complxity: O(N*N*N) Space Complexity: O(1)
 int countVowelSubstrings(string s) {
         int n=s.length();
@@ -36,3 +36,27 @@ int countVowelSubstrings(string s) {
         }
         return ans;
     }
+
+// Approach 2:  Time Complexity: O(N*N) Space Complexity: O(5)
+// Here again we are finding the substring in O(N*N) and we are determining  if the current substring can contribute to the answer or not
+class Solution {
+public:
+    bool isVowel(char c){
+        return c=='a' || c=='e' || c=='i' || c=='o' || c=='u';
+    }
+    int countVowelSubstrings(string s) {
+        int n=s.length();
+        int ans=0;
+        for(int i=0;i<n;i++){ // substring starting from i and ending at j
+            set<char> st;
+            for(int j=i;j<n;j++){
+                if(!isVowel(s[j]))
+                    break;
+                st.insert(s[j]);
+                if(st.size()==5)
+                    ans++;
+            }
+        }
+        return ans;
+    }
+};
