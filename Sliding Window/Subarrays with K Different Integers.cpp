90cb5c493bb73e1bfb97751c34409b1bccfda4f6
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-11-08 00:58:31 +0530

    Create Subarrays with K Different Integers.cpp

---
+++ Sliding Window/Subarrays with K Different Integers.cpp
@@ -0,0 +1,29 @@
+/*
+Problem Link: https://leetcode.com/problems/subarrays-with-k-different-integers/
+Same as:  https://leetcode.com/problems/count-vowel-substrings-of-a-string/
+
+*/
+// Time Complexity: O(N) Space Complexity:O(N)
+
+    int solve(vector<int> &arr,int k){
+        map<int,int> m;
+        int ans=0,i=0,j=0,n=arr.size();
+        while(j<n){
+            m[arr[j]]++;
+            if(m.size()>k){
+                while(m.size()>k){
+                    m[arr[i]]--;
+                    if(m[arr[i]]==0)
+                        m.erase(arr[i]);
+                    i++;
+                }
+            }
+            ans+=(j-i+1);
+            j++;
+        }
+        return ans;
+    }
+    int subarraysWithKDistinct(vector<int>& arr, int k) {
+        int ans=solve(arr,k)-solve(arr,k-1);
+        return ans;
+    }
