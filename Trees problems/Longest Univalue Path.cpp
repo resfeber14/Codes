e0cb5e3b5f00e00f9ebffaa2a65efaf32074af4b
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-11-11 16:12:35 +0530

    Create Longest Univalue Path.cpp

---
+++ Trees problems/Longest Univalue Path.cpp
@@ -0,0 +1,37 @@
+/*
+Problem Link: https://leetcode.com/problems/longest-univalue-path/
+
+*/
+// Time complexity: O(N*N) Space Complexity: O(1)
+
+
+int solve(TreeNode* root,int &ans){
+        if(!root)
+            return 0;
+        int cnt1=0,cnt2=0;
+        if(root->left && root->left->val==root->val)
+            cnt1+=solve(root->left,ans);
+        if(root->right && root->right->val==root->val)
+            cnt2+=solve(root->right,ans);
+        int op1=cnt1+cnt2+1; // if the path passes through the root
+        int op2=cnt1+1; // if the path lies in the left 
+        int op3=cnt2+1; // if the path lies in the left 
+        ans=max({op1,op2,op3,ans}); 
+        int x=max(op2,op3); // take maximum of two paths
+        return x;
+    }
+    
+    void solve1(TreeNode* root,int &ans){
+        if(!root)
+            return;
+        solve(root,ans);
+        solve1(root->left,ans);
+        solve1(root->right,ans);
+    }
+    int longestUnivaluePath(TreeNode* root) {
+        int ans=0;
+        solve1(root,ans);
+        if(ans>0) // we want no of edges not no of nodes.
+            ans--;
+        return ans;
+    }
